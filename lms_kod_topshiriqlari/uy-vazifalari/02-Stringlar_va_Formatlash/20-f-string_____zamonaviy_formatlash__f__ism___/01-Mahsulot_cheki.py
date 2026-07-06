152507b5f0a71d2d3bd34ad72561587840d4e290
elchibekovazinara-a11y <elchibekovazinara@gmail.com>
2026-07-06 12:31:04 +0500

    Add: Mahsulot cheki [elchibekovazinara-a11y]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/20-f-string_____zamonaviy_formatlash__f__ism___/01-Mahsulot_cheki.py
@@ -0,0 +1,10 @@
+mahsulot_nomi = input()
+narxi = int(input())
+soni = int(input())
+
+jami_summa = narxi * soni
+
+print(f"Mahsulot: {mahsulot_nomi}")
+print(f"Narxi: {narxi} som")
+print(f"Soni: {soni}")
+print(f"Jami: {jami_summa} som")
