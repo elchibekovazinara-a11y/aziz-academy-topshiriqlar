71e1ca646fd5cc468b25f4ffe8f8251e8966b45f
elchibekovazinara-a11y <elchibekovazinara@gmail.com>
2026-07-10 21:35:44 +0500

    Add: Login va parol tekshiruvi [elchibekovazinara-a11y]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Login_va_parol_tekshiruvi.py
@@ -0,0 +1,12 @@
+login = input()
+parol = input()
+
+if login == "admin":
+    
+    if parol == "1234":
+        print("Xush kelibsiz")
+    else:
+        print("Parol xato")
+else:
+    
+    print("Login topilmadi")
