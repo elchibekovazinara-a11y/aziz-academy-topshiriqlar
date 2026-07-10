c316460fa0550a959f99eff37602d942a4a4660b
elchibekovazinara-a11y <elchibekovazinara@gmail.com>
2026-07-10 08:17:23 +0500

    Add: Uch sondan eng kattasi [elchibekovazinara-a11y]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/01-if___elif___else_____shart_operatorlari_asoslari/01-Uch_sondan_eng_kattasi.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+
+eng_katta = a
+
+
+if b > eng_katta:
+    eng_katta = b
+
+if c > eng_katta:
+    eng_katta = c
+    
+print(eng_katta)
+    
