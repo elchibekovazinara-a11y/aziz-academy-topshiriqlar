f92ba257afd802e15581f68a8afd0cac1a4d6ee5
elchibekovazinara-a11y <elchibekovazinara@gmail.com>
2026-07-11 19:52:48 +0500

    Add: Kabisa yilni aniqlash [elchibekovazinara-a11y]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Kabisa_yilni_aniqlash.py
@@ -0,0 +1,16 @@
+yil = int(input())
+
+if yil % 4 == 0:
+    
+    if yil % 100 == 0:
+        
+        if yil % 400 == 0:
+            print("Kabisa")
+        else:
+            print("Kabisa emas")
+    else:
+        
+        print("Kabisa")
+else:
+    
+    print("Kabisa emas")
