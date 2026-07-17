168c653f9d2de968d9e3da3c95b8d8ef83be9e9d
elchibekovazinara-a11y <elchibekovazinara@gmail.com>
2026-07-17 08:41:55 +0500

    Add: Musbat va manfiylar soni [elchibekovazinara-a11y]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/09-Filtrlash___juft_sonlar__musbat_sonlar_ajratish/01-Musbat_va_manfiylar_soni.py
@@ -0,0 +1,12 @@
+N = int(input())
+musbat = 0
+manfiy = 0
+
+for _ in range(N):
+    son = int(input())
+    if son > 0:
+        musbat += 1
+    elif son < 0:
+        manfiy += 1
+        
+print(musbat, manfiy)
